bf117154ab080edd112cd6b42b8a4aadfc10c73f
Penguin60 <radean.rashed@gmail.com>
2025-07-19 00:38:19 -0400

    Add base ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+
 export default function Home() {
   return (
-    <div className="bg-background h-screen">
-      <h1 className="text-foreground"> Hello </h1>
+    <div className="bg-background h-screen flex flex-col items-center justify-center p-4">
+      <div className="w-full max-w-md space-y-4">
+        <Input placeholder="paste..." className="w-full" />
+        <div className="flex items-center justify-between">
+          <ToggleGroup type="single" defaultValue="text" className="justify-start">
+            <ToggleGroupItem value="text">text</ToggleGroupItem>
+            <ToggleGroupItem value="video">video</ToggleGroupItem>
+          </ToggleGroup>
+          <Button type="submit" variant="outline" className="">go</Button>
+        </div>
+      </div>
     </div>
   );
 }
